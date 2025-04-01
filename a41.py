86508fe5efd9b289be03718b5d51ee7e41456c94
ee2727 <changchangtasi@gmail.com>
2025-04-01 16:42:54 +0800

    Update a041.py

--- a41.py
+++ a41.py
@@ -7,5 +7,13 @@ ans= random.randint(start, end)
 while True :
     guess = input(f'請輸入你要猜的數字，介於｛start}與{end｝之間：'）
 
+    try:
+         guess = int（guess)
+    except valueError:
+         print('你輸入的不是一個數字'）
+         continue
+        
+    print('是合法的'）
+    
 
  
