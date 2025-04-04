03fb2c5ee5d65c2a1a5c1eac9f9a1839d8e06b5f
ee2727 <changchangtasi@gmail.com>
2025-04-04 15:39:01 +0800

    更新 a41.py

--- a41.py
+++ a41.py
@@ -1,10 +1,10 @@
 import random
-
+def play():
 start = 1
 end = 100
 ans= random.randint(start, end)
 
-guess_count
+guess_count = 0
 while True :
     guess = input(f'請輸入你要猜的數字，介於｛start}與{end｝之間：'）
 
@@ -13,24 +13,28 @@ while True :
     except valueError:
          print('你輸入的不是一個數字'）
          continue
+    
     if not (start <= guess<= end):
-       print (　'你輸入的數字不在合法範圍中')
-       continue
+         print (　'你輸入的數字不在合法範圍中')
+         continue
+        
+    guess_count += 1
     
     if guess==ans:
-       print('恭喜你答對了'）
-       break
+         print('恭喜你答對了'）
+         break
     
     if guess< ans
-       start = guess+1
+         start = guess+1
     else:
-       end = guess -1
+         end = guess -1
     if start == end:
-        print(f'答案是｛ans}')
-        break
+         print(f'答案是｛ans}')
+         guess_count += 1
+         break
         
 
-    print('是合法的'）
+    print(f'你花了{guess_count}次猜到正確答案'）
     
 
  
